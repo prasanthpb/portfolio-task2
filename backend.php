c543d549afaa6ba4593838b1f1c2fdfbe55a4c62
prasanthpb <116637557+prasanthpb@users.noreply.github.com>
2024-02-07 11:28:51 +0530

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,27 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+
+
+  $email_from = 'yourname@yourwebsite.com';
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+
+
+                            $to = "name1@website-name.com, name2@website-name.com,name3@website-name.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+$headers .= "Cc: someone@domain.com \r\n";
+
+$headers .= "Bcc: someoneelse@domain.com \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+?>
